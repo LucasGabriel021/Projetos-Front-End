8f09200526f14628d8b671654b69e1d9bfb9fabb
Lucas Gomes de Oliveira <lucasgabrielgomes33@gmail.com>
2023-01-05 13:39:39 -0300

    patch 1.0

---
+++ Projetos/Robotron-2000/-robotron-2000-aula_2/js/main.js
@@ -0,0 +1,84 @@
+/**
+ * Objeto de estatisticas!
+ */
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+
+const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+//console.log(estatisticas);
+
+controle.forEach(function(item, index) {
+    //console.log(item);
+    item.addEventListener("click", function(e) {
+        //console.log(e.target.textContent);
+        //console.log(e.target.parentNode);
+        //console.log(e);
+        manipulaDados(e.target.dataset.controle, e.target.parentNode); // TextContent propriedade que irá obter o valor do nó
+        atualizaEstatistica(e.target.dataset.peca);
+    })
+});
+
+/*
+adicionar.addEventListener("click", () => manipulaDados("soma"));
+
+subtrair.addEventListener("click", () => manipulaDados("tira"));
+*/
+
+function manipulaDados(operacao, controle) {
+    const custom = controle.querySelector("[data-contador]");
+    if(operacao === "+") {
+        custom.value = parseInt(custom.value) + 1;
+    } else if (operacao === "-") {
+        custom.value = parseInt(custom.value) - 1;
+    }
+}
+
+function atualizaEstatistica(peca) {
+    console.log(peca);
+    estatisticas.forEach((item) => {
+        item.textContent = parseInt(item.textContent) + pecas[peca][item.dataset.estatisticas];
+    });
+}
+
+/*
+var lista = ["Laranja", "Vermelho", "Branco", "Amarelo", "Rosa"]; 
+let posicao = lista.indexOf("Vermelho");
+console.log("Posição do vermelho: "+posicao);
+if(posicao === 1) {
+    lista.splice(1, 1);
+}
+console.log(lista);
+*/
